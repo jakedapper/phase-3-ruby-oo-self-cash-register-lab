fe712c19b9bbf5fe89305376e9b967a8c618875c
jake dapper <jakedapper@gmail.com>
2022-08-30 16:20:17 -0500

    commit tree 054b5ded957ed7b1a0abb09c9ffe848308d9e9c0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+require 'pry'
+class CashRegister
 
+attr_accessor :total
+attr_reader :discount
+attr_writer :dollars
+
+  def initialize (discount=0)
+    @discount = discount
+    @total = 0
+  end
+
+  def add_item(title, price, amount = 1)
+    @total += price * amount
+  end
+
+  def apply_discount
+    if discount > 0
+      my_discount = @total * @discount/100
+      @total -= my_discount
+      "After the discount, the total comes to $#{total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items(item, price, amount)
+    items = []
+    items << item
+  end
+
+end
